d4ffcde977731d3cc499751c7dc21be56aa410b7
ngiyshhk <nagai_yoshihiko_gn@cyberagent.co.jp>
2017-04-04 12:44:00 +0900

    Update chatwork.js

--- chatwork.js
+++ chatwork.js
@@ -1,2 +1,15 @@
 var allRead = function(){$("#_roomListItems > li").has("li._unreadBadge").each(function(i, o){o.click()});};
-$("#_chatSendToolbar").append('<input type="button" value="全既読" onClick="allRead()"/>');
+var sample = function(){
+  var user = $(".mainContent p._speakerName.chatName:last > span");
+
+  var id = user.attr("class").replace(/[^\d]/g, "");
+  var name = user.text();
+
+  var header = "[To:" + id + "]" + name + "さん\n";
+  var message = "サンプル添付いたします。\nご確認よろしくお願いいたします。";
+  $("#_chatText").val(header + message).change();
+};
+
+$("#_chatSendToolbar")
+  .append('<input type="button" value="全既読" onClick="allRead()"/>')
+  .append('<input type="button" value="サンプル" onClick="sample()"/>');
